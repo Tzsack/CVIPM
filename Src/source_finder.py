5140ab93a49eaefa2d793cdfac4ab40750b20ab6
graitanto21 <graitanto21 antoniograsso21@gmail.com>
2019-03-28 11:06:11 +0100

    refactoring

--- Src/source_finder.py
+++ Src/source_finder.py
@@ -18,7 +18,33 @@ class SourceFinder:
         self.matrix = Util.from_fits_to_mat(fits_file_name)
         self.wcs = WCS(fits_file_name)
 
-    def source_pixels(self, sigmas):
+    @staticmethod
+    def best_candidate(isolatedness_values, k=1.5, quorum=0.3):
+        candidates = []
+        votes = []
+        total_votes = 0
+
+        for step in range(0, len(isolatedness_values[0]) - 2):
+            values = Util.project_list(isolatedness_values, step + 2)
+            sorted_values = sorted(values, reverse=True)
+            if sorted_values[0] >= sorted_values[1] * k:
+                if candidates.count(isolatedness_values[values.index(sorted_values[0])][1]) == 0:
+                    candidates.append(isolatedness_values[values.index(sorted_values[0])][1])
+                    votes.append(1)
+                else:
+                    votes[candidates.index(isolatedness_values[values.index(sorted_values[0])][1])] += 1
+                total_votes += 1
+
+        if not votes:
+            return None
+
+        sorted_votes = sorted(votes, reverse=True)
+        if sorted_votes[0] >= (len(isolatedness_values[0]) - 2) * quorum:
+            return candidates[votes.index(sorted_votes[0])][1], candidates[votes.index(sorted_votes[0])][0]
+        else:
+            return None
+
+    def compute_isolatedness(self, sigmas, maxima_per_iter=3, furthest_index=2, dist_thresh=4):
         """Search the source pixels position in the image"""
 
         smoothed = None
@@ -29,50 +55,23 @@ class SourceFinder:
             sigma = sigmas[step]
             if last_sigma == 0 or sigma <= last_sigma:
                 smoothed = cv.GaussianBlur(self.matrix, Util.kernel_size(sigma), sigma)
-                last_sigma = sigma
             else:
                 sigma_to_add = math.sqrt(pow(sigma, 2) - pow(last_sigma, 2))
                 smoothed = cv.GaussianBlur(smoothed, Util.kernel_size(sigma_to_add), sigma_to_add)
-                last_sigma = sigma
+            last_sigma = sigma
             maxima = Util.local_maxima(smoothed)
             if sigma >= 0.5:
                 maxima = gi.optimize_maxima(smoothed, maxima, sigma)
             maxima = Util.sort_list(maxima, 1, True)
 
-            # Trying to create a synthetic version of the image with less background noise
-
-            # cv.imshow(str(round(sigma, 2)), smoothed * 1/np.max(smoothed))
-
-            # dist_threshold = 5
-
-            # out = np.copy(smoothed)
-            # for x in range(0, len(out)):
-            #     print(x)
-            #     for y in range(0, len(out[x])):
-            #         out[x][y] = 0
-            #         for m in maxima:
-            #             out[x][y] += m[1] * gi.gaussian((x, y), m[0], sigma)
-            #
-            # cv.imshow("max " + str(round(sigma, 2)), out * 1/np.max(out))
-
-
-            # Construction of a data matrix containing for each local maximum the corrispondig values of isolatedness
-            # for increasing values of sigma
-
-            last_max = 3
-            last_d = 2
-            dist_thresh = 4
-
-            for maximum_a in maxima[:last_max]:
+            for maximum_a in maxima[:maxima_per_iter]:
                 distances = []
                 for maximum_b in maxima:
                     distances.append(Util.distance_eu(maximum_a[0], maximum_b[0]))
-                distances.sort(reverse=False)
+                distances.sort()
                 isolatedness = 0
-                for d in distances[1:last_d]:
+                for d in distances[1:furthest_index]:
                     isolatedness += d
-
-                # data matrix construction
 
                 found = False
                 for max_id in range(0, len(data)):
@@ -87,42 +86,16 @@ class SourceFinder:
                         data[-1].append(float(0))
                     data[-1][step + 2] = round(isolatedness, 2)
 
-        candidates = []
-        votes = []
-        k = 1.5
-        quorum = 3/10
-        total_votes = 0
-
-        for step in range(0, len(sigmas)):
-            values = Util.project_list(data, step + 2)
-            sorted_values = sorted(values, reverse=True)
-            if sorted_values[0] >= sorted_values[1] * k:
-                if candidates.count(data[values.index(sorted_values[0])][1]) == 0:
-                    candidates.append(data[values.index(sorted_values[0])][1])
-                    votes.append(1)
-                else:
-                    votes[candidates.index(data[values.index(sorted_values[0])][1])] += 1
-                total_votes += 1
-
-        sorted_votes = sorted(votes, reverse=True)
-
-        print(candidates)
-        print(votes)
-
-        if sorted_votes[0] >= len(sigmas) * quorum:
-            print(candidates[votes.index(sorted_votes[0])])
-        else:
-            print("No source")
-
+        """
         for row in data:
             print(*row, sep="\t")
             plt.plot(sigmas, row[2:], label=str(int(row[0][0]))+", "+str(int(row[0][1])))
             plt.legend()
 
         plt.figure()
+        """
 
-        return 0, 0
-
+        return data
 
 """
                     SIGMA
@@ -147,52 +120,26 @@ def main(index):
     index -= 1
     sf = SourceFinder(skymaps[index])
     print(skymaps[index])
-    pixels_coord = sf.source_pixels([2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3])
+    isolatedness_values = sf.compute_isolatedness([2, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3])
+    pixels_coord = sf.best_candidate(isolatedness_values)
     # print(pixels_coord)
-    #lon, lat = Util.from_pix_to_wcs(pixels_coord, sf.wcs)
-    #print(lon, lat)
-    #plt.show()
-    os.chdir(curdir)
-
-
-def main2(indexes, sigma):
-    curdir = os.getcwd()
-    os.chdir("../Skymaps")
-    skymaps = os.listdir(".")
-    skymaps.sort()
-
-    for index in indexes:
-        sf = SourceFinder(skymaps[index - 1])
-        mat = cv.GaussianBlur(sf.matrix, Util.kernel_size(sigma), sigma)
-        maxima = Util.local_maxima(mat)
-        # print(maxima)
-        maxima = gi.optimize_maxima(mat, maxima, sigma)
-        # print(maxima)
-        # cv.imshow(skymaps[index - 1] + str(index), Util.flip_mat(mat))
-        Util.sort_list(maxima, 1, True)
-        print(skymaps[index - 1], maxima)
-
-        maxima = Util.project_list(maxima, 1)
-        for i in range(0, len(maxima) - 1):
-            maxima[i] = maxima[i] - maxima[i + 1]
-
-        plt.plot(maxima, label=''+str(index))
-        plt.legend()
-        # cv.waitKey(0)
-        # cv.destroyAllWindows()
-
+    if pixels_coord:
+        src_cor = np.round(Util.from_pix_to_wcs(pixels_coord, sf.wcs), 2)
+        print(src_cor[0], src_cor[1])
+    else:
+        print("No source found")
+    # plt.show()
     os.chdir(curdir)
 
 
 # main
-
-# main(1)   # 145, 76
-# main(2)   # 150, 117
-# main(3)   # 86,  62
-# main(4)   # 116, 73
-# main(5)   # 121, 112
-# main(6)   # 155, 84
-# main(7)   # 142, 97
+main(1)   # 145, 76
+main(2)   # 150, 117
+main(3)   # 86,  62
+main(4)   # 116, 73
+main(5)   # 121, 112
+main(6)   # 155, 84
+main(7)   # 142, 97
 main(8)   # 91, 131
 main(9)   # 97, 93
 main(10)  # 86, 121 (?)
@@ -207,4 +154,3 @@ plt.show()
 #     main2([4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 1*s + 0.5)
 #     plt.figure()
 # plt.show()
-
